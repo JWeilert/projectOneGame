33d173aff69f7c90493ed5d9b8286817805b8d2b
John Weilert <mattweilert@gmail.com>
2022-05-10 21:29:58 -0500

    Added Event Lisener

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+let container = document.querySelector("body");
+
+let firstHalf = document.getElementById("firstHalf");
+
+let secondHalf = document.getElementById("firstHalf");
+
+let dice = document.getElementsByClassName("dice")[0];
+
+dice.addEventListener("click", function (event) {
+  event.stopPropagation();
+  console.log("test");
+  //   if ((getElementById = P1dice1)) {
+  //     console.log("YAY");
+  //     console.log(getElementById);
+  //   }
+});
+
 // Create 2 arrays array that can each hold 5 object dice
 
 let playerOneDice = [
-  { number: roll(), reRoll: false },
   { number: roll(), reRoll: false },
   { number: roll(), reRoll: false },
   { number: roll(), reRoll: false },
@@ -15,12 +31,33 @@ let playerTwoDice = [
   { number: roll(), reRoll: false },
   { number: roll(), reRoll: false },
   { number: roll(), reRoll: false },
-  { number: roll(), reRoll: false },
 ];
+
+// Creates random number 0-5
 
 function roll() {
   return Math.floor(Math.random() * 6);
+  console.log("Rolled");
 }
 
-console.log(playerOneDice);
-console.log(playerTwoDice);
+// Test to see everything run
+
+function displayPlayerOne() {
+  for (let i = 0; i < playerOneDice.length; i++) {
+    let diceContainer = document.getElementById(`P1dice${i}`);
+    diceContainer.innerHTML = playerOneDice[i].number;
+  }
+}
+
+function displayPlayerTwo() {
+  for (let i = 0; i < playerTwoDice.length; i++) {
+    let diceRoll = playerTwoDice[i].number;
+    let diceContainer = document.getElementById(`P2dice${i}`);
+    diceContainer.innerHTML = playerTwoDice[i].number;
+  }
+}
+
+displayPlayerOne();
+displayPlayerTwo();
+// console.log(playerOneDice);
+// console.log(playerTwoDice);
