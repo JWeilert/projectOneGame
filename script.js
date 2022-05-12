219e814b0b40974095df0bd30544b3eb5afe07e4
John Weilert <mattweilert@gmail.com>
2022-05-11 20:25:01 -0500

    ReRoll switches states when dice is clicked

--- script.js
+++ script.js
@@ -12,18 +12,35 @@ let dice = document.getElementsByClassName("dice");
 
 for (let i = 0; i < dice.length; i++) {
   dice[i].addEventListener("click", function (event) {
-    console.log(this.id);
+    let diceID = this.id;
+    console.log(diceID);
+    if (diceID === "P1dice1") {
+      playerOneDice[0].reRoll = !playerOneDice[0].reRoll;
+      console.log(playerOneDice[0]);
+    } else if (diceID === "P1dice2") {
+      playerOneDice[1].reRoll = !playerOneDice[1].reRoll;
+      console.log(playerOneDice[1]);
+    } else if (diceID === "P1dice3") {
+      playerOneDice[2].reRoll = !playerOneDice[2].reRoll;
+      console.log(playerOneDice[2]);
+    } else if (diceID === "P1dice4") {
+      playerOneDice[3].reRoll = !playerOneDice[3].reRoll;
+      console.log(playerOneDice[3]);
+    } else if (diceID === "P1dice5") {
+      playerOneDice[4].reRoll = !playerOneDice[4].reRoll;
+      console.log(playerOneDice[4]);
+    }
   });
 }
 
 // Create 2 arrays array that can each hold 5 object dice
 
 let playerOneDice = [
-  { number: roll(), reRoll: false },
-  { number: roll(), reRoll: false },
-  { number: roll(), reRoll: false },
-  { number: roll(), reRoll: false },
-  { number: roll(), reRoll: false },
+  { number: roll(), reRoll: true },
+  { number: roll(), reRoll: true },
+  { number: roll(), reRoll: true },
+  { number: roll(), reRoll: true },
+  { number: roll(), reRoll: true },
 ];
 
 let playerTwoDice = [
@@ -34,10 +51,10 @@ let playerTwoDice = [
   { number: roll(), reRoll: false },
 ];
 
-// Creates random number 0-5
+// Creates random number 1-6
 
 function roll() {
-  return Math.floor(Math.random() * 6);
+  return Math.floor(Math.random() * 6 + 1);
   console.log("Rolled");
 }
 
@@ -45,7 +62,7 @@ function roll() {
 
 function displayPlayerOne() {
   for (let i = 0; i < playerOneDice.length; i++) {
-    let diceContainer = document.getElementById(`P1dice${i}`);
+    let diceContainer = document.getElementById(`P1dice${i + 1}`);
     diceContainer.innerHTML = playerOneDice[i].number;
   }
 }
@@ -53,7 +70,7 @@ function displayPlayerOne() {
 function displayPlayerTwo() {
   for (let i = 0; i < playerTwoDice.length; i++) {
     let diceRoll = playerTwoDice[i].number;
-    let diceContainer = document.getElementById(`P2dice${i}`);
+    let diceContainer = document.getElementById(`P2dice${i + 1}`);
     diceContainer.innerHTML = playerTwoDice[i].number;
   }
 }
