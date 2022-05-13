f87f104dc1c5675f23907e9f6ca8e38aa237383d
John Weilert <mattweilert@gmail.com>
2022-05-12 20:30:48 -0500

    ReRoll and dice visual apearance works

--- script.js
+++ script.js
@@ -1,37 +1,4 @@
-// Just making variables for future use
-
-let container = document.querySelector("body");
-
-let firstHalf = document.getElementById("firstHalf");
-
-let secondHalf = document.getElementById("firstHalf");
-
-// Add Event listener to dice. Also Selects ID
-
-let dice = document.getElementsByClassName("dice");
-
-for (let i = 0; i < dice.length; i++) {
-  dice[i].addEventListener("click", function (event) {
-    let diceID = this.id;
-    console.log(diceID);
-    if (diceID === "P1dice1") {
-      playerOneDice[0].reRoll = !playerOneDice[0].reRoll;
-      console.log(playerOneDice[0]);
-    } else if (diceID === "P1dice2") {
-      playerOneDice[1].reRoll = !playerOneDice[1].reRoll;
-      console.log(playerOneDice[1]);
-    } else if (diceID === "P1dice3") {
-      playerOneDice[2].reRoll = !playerOneDice[2].reRoll;
-      console.log(playerOneDice[2]);
-    } else if (diceID === "P1dice4") {
-      playerOneDice[3].reRoll = !playerOneDice[3].reRoll;
-      console.log(playerOneDice[3]);
-    } else if (diceID === "P1dice5") {
-      playerOneDice[4].reRoll = !playerOneDice[4].reRoll;
-      console.log(playerOneDice[4]);
-    }
-  });
-}
+var playerOneTurn = true;
 
 // Create 2 arrays array that can each hold 5 object dice
 
@@ -44,21 +11,127 @@ let playerOneDice = [
 ];
 
 let playerTwoDice = [
-  { number: roll(), reRoll: false },
-  { number: roll(), reRoll: false },
-  { number: roll(), reRoll: false },
-  { number: roll(), reRoll: false },
-  { number: roll(), reRoll: false },
+  { number: roll(), reRoll: true },
+  { number: roll(), reRoll: true },
+  { number: roll(), reRoll: true },
+  { number: roll(), reRoll: true },
+  { number: roll(), reRoll: true },
 ];
+
+// Just making variables for future use
+
+let container = document.querySelector("body");
+
+let firstHalf = document.getElementById("firstHalf");
+
+let secondHalf = document.getElementById("firstHalf");
+
+//Test for reRolling
+
+let topUi = document.getElementById("topUi");
+
+topUi.addEventListener("click", function (event) {
+  for (let i = 0; i < playerOneDice.length; i++) {
+    if (playerOneDice[i].reRoll == true) {
+      playerOneDice[i].number = roll();
+    } else console.log("Not reRolled");
+  }
+  displayPlayerOne();
+});
+
+let bottomUi = document.getElementById("bottomUi");
+
+bottomUi.addEventListener("click", function (event) {
+  for (let i = 0; i < playerTwoDice.length; i++) {
+    if (playerTwoDice[i].reRoll == true) {
+      playerTwoDice[i].number = roll();
+    } else console.log("Not reRolled");
+  }
+  displayPlayerTwo();
+});
+
+// Add Event listener to dice. Also Selects ID
+
+let dice = document.getElementsByClassName("dice");
+
+for (let i = 0; i < dice.length; i++) {
+  dice[i].addEventListener("click", function () {
+    let diceID = this.id;
+    reRollValue(diceID);
+  });
+}
+
+// Changes value of ReRoll
+
+function reRollValue(diceID) {
+  let selector = document.getElementById(diceID);
+  if (diceID === "P1dice1") {
+    playerOneDice[0].reRoll = !playerOneDice[0].reRoll;
+    console.log(playerOneDice[0]);
+    let diceSelected = playerOneDice[0];
+    display(diceSelected, selector);
+  } else if (diceID === "P1dice2") {
+    playerOneDice[1].reRoll = !playerOneDice[1].reRoll;
+    console.log(playerOneDice[1]);
+    let diceSelected = playerOneDice[1];
+    display(diceSelected, selector);
+  } else if (diceID === "P1dice3") {
+    playerOneDice[2].reRoll = !playerOneDice[2].reRoll;
+    console.log(playerOneDice[2]);
+    let diceSelected = playerOneDice[2];
+    display(diceSelected, selector);
+  } else if (diceID === "P1dice4") {
+    playerOneDice[3].reRoll = !playerOneDice[3].reRoll;
+    console.log(playerOneDice[3]);
+    let diceSelected = playerOneDice[3];
+    display(diceSelected, selector);
+  } else if (diceID === "P1dice5") {
+    playerOneDice[4].reRoll = !playerOneDice[4].reRoll;
+    console.log(playerOneDice[4]);
+    let diceSelected = playerOneDice[4];
+    display(diceSelected, selector);
+  } else if (diceID === "P2dice1") {
+    playerTwoDice[0].reRoll = !playerTwoDice[0].reRoll;
+    console.log(playerTwoDice[0]);
+    let diceSelected = playerTwoDice[0];
+    display(diceSelected, selector);
+  } else if (diceID === "P2dice2") {
+    playerTwoDice[1].reRoll = !playerTwoDice[1].reRoll;
+    console.log(playerTwoDice[1]);
+    let diceSelected = playerTwoDice[1];
+    display(diceSelected, selector);
+  } else if (diceID === "P2dice3") {
+    playerTwoDice[2].reRoll = !playerTwoDice[2].reRoll;
+    console.log(playerTwoDice[2]);
+    let diceSelected = playerTwoDice[2];
+    display(diceSelected, selector);
+  } else if (diceID === "P2dice4") {
+    playerTwoDice[3].reRoll = !playerTwoDice[3].reRoll;
+    console.log(playerTwoDice[3]);
+    let diceSelected = playerTwoDice[3];
+    display(diceSelected, selector);
+  } else if (diceID === "P2dice5") {
+    playerTwoDice[4].reRoll = !playerTwoDice[4].reRoll;
+    console.log(playerTwoDice[4]);
+    let diceSelected = playerTwoDice[4];
+    display(diceSelected, selector);
+  }
+}
+
+function display(diceSelected, selector) {
+  if (diceSelected.reRoll == false) {
+    selector.style.color = "red";
+  } else selector.style.color = "blue";
+}
 
 // Creates random number 1-6
 
 function roll() {
-  return Math.floor(Math.random() * 6 + 1);
   console.log("Rolled");
+  return Math.floor(Math.random() * 6 + 1);
 }
 
-// Test to see numbers apear on dice
+// Displays Dice
 
 function displayPlayerOne() {
   for (let i = 0; i < playerOneDice.length; i++) {
@@ -77,5 +150,3 @@ function displayPlayerTwo() {
 
 displayPlayerOne();
 displayPlayerTwo();
-// console.log(playerOneDice);
-// console.log(playerTwoDice);
