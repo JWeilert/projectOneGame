24cc3cc248bffd53bae6f89c96a7da5d746133b3
John Weilert <mattweilert@gmail.com>
2022-05-14 20:38:08 -0500

    Added Turns

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 var started = true;
 
-//Sets volume of music
-let backgroundMusic = document.getElementById("backgroundMusic");
-let backgroundSound = document.getElementById("backgroundNoise");
-backgroundMusic.play();
-backgroundSound.play();
+var playerOneTurn = false;
+var playerOneRolls = 0;
+var playerTwoRolls = 0;
 
-backgroundMusic.volume = 0.4;
-backgroundSound.volume = 0.3;
+//Sets volume of music
+document.getElementById("backgroundMusic").volume = 0.2;
+document.getElementById("backgroundNoise").volume = 0.1;
+
+// backgroundMusic.play();
+// backgroundSound.play();
+
+// backgroundMusic.volume = 0.4;
+// backgroundSound.volume = 0.3;
 
 var oneDice = new Audio("assets/Audio/oneDice.mp3");
 var twoDice = new Audio("assets/Audio/twoDice.mp3");
 var multiDice = new Audio("assets/Audio/multiDice.mp3");
-
-var playerOneTurn = true;
 
 // Create 2 arrays array that can each hold 5 object dice
 
@@ -23,9 +26,11 @@ let playerOneDice = [
   { number: roll(), reRoll: true },
   { number: roll(), reRoll: true },
   { number: roll(), reRoll: true },
+  { number: roll(), reRoll: true },
 ];
 
 let playerTwoDice = [
+  { number: roll(), reRoll: true },
   { number: roll(), reRoll: true },
   { number: roll(), reRoll: true },
   { number: roll(), reRoll: true },
@@ -46,31 +51,59 @@ let secondHalf = document.getElementById("firstHalf");
 let rollButtonTop = document.getElementById("rollButtonTop");
 
 rollButtonTop.addEventListener("click", function (event) {
-  start = false;
-  let diceRolled = 0;
-  for (let i = 0; i < playerOneDice.length; i++) {
-    if (playerOneDice[i].reRoll == true) {
-      diceRolled += 1;
-      playerOneDice[i].number = roll();
-    } else console.log("No reroll");
-  }
-  displayPlayerOne();
-  diceRolledAudio(diceRolled);
+  if (playerOneTurn == true && playerOneRolls < 3) {
+    playerOneRolls += 1;
+    let diceRolled = 0;
+    //audio and reReoll check
+    for (let i = 0; i < playerOneDice.length; i++) {
+      if (playerOneDice[i].reRoll == true) {
+        diceRolled += 1;
+        playerOneDice[i].number = roll();
+        //Needs touched up
+        if (playerOneRolls >= 3) {
+          console.log(playerOneRolls);
+          for (let i = 0; i < playerOneDice.length; i++) {
+            playerOneDice[i].reRoll == false;
+            let id = `P1dice${i + 1}`;
+            let selector = document.getElementById(id);
+            selector.classList.remove("reRoll");
+          }
+        }
+      }
+      displayPlayerOne();
+      diceRolledAudio(diceRolled);
+      playerOneTurn = false;
+    }
+  } else alert("Not your turn");
 });
 
 let rollButtonBottom = document.getElementById("rollButtonBottom");
 
 rollButtonBottom.addEventListener("click", function (event) {
-  let diceRolled = 0;
-  started = false;
-  for (let i = 0; i < playerTwoDice.length; i++) {
-    if (playerTwoDice[i].reRoll == true) {
-      diceRolled += 1;
-      playerTwoDice[i].number = roll();
-    } else console.log("Not reRolled");
-  }
-  displayPlayerTwo();
-  diceRolledAudio(diceRolled);
+  if (playerOneTurn == false && playerTwoRolls < 3) {
+    playerTwoRolls += 1;
+    let diceRolled = 0;
+    //audio and reReoll check
+    for (let i = 0; i < playerTwoDice.length; i++) {
+      if (playerTwoDice[i].reRoll == true) {
+        diceRolled += 1;
+        playerTwoDice[i].number = roll();
+        //Needs touched up
+        if (playerTwoRolls >= 3) {
+          console.log(playerTwoRolls);
+          for (let i = 0; i < playerTwoDice.length; i++) {
+            playerTwoDice[i].reRoll == false;
+            let id = `P2dice${i + 1}`;
+            let selector = document.getElementById(id);
+            selector.classList.remove("reRoll");
+          }
+        }
+      }
+      displayPlayerTwo();
+      diceRolledAudio(diceRolled);
+      playerOneTurn = true;
+    }
+  } else alert("Not your turn");
 });
 
 function diceRolledAudio(diceRolled) {
@@ -102,56 +135,70 @@ for (let i = 0; i < dice.length; i++) {
 
 function reRollValue(diceID) {
   let selector = document.getElementById(diceID);
-  if (diceID === "P1dice1") {
-    playerOneDice[0].reRoll = !playerOneDice[0].reRoll;
-    console.log(playerOneDice[0]);
-    let diceSelected = playerOneDice[0];
-    display(diceSelected, selector);
-  } else if (diceID === "P1dice2") {
-    playerOneDice[1].reRoll = !playerOneDice[1].reRoll;
-    console.log(playerOneDice[1]);
-    let diceSelected = playerOneDice[1];
-    display(diceSelected, selector);
-  } else if (diceID === "P1dice3") {
-    playerOneDice[2].reRoll = !playerOneDice[2].reRoll;
-    console.log(playerOneDice[2]);
-    let diceSelected = playerOneDice[2];
-    display(diceSelected, selector);
-  } else if (diceID === "P1dice4") {
-    playerOneDice[3].reRoll = !playerOneDice[3].reRoll;
-    console.log(playerOneDice[3]);
-    let diceSelected = playerOneDice[3];
-    display(diceSelected, selector);
-  } else if (diceID === "P1dice5") {
-    playerOneDice[4].reRoll = !playerOneDice[4].reRoll;
-    console.log(playerOneDice[4]);
-    let diceSelected = playerOneDice[4];
-    display(diceSelected, selector);
-  } else if (diceID === "P2dice1") {
-    playerTwoDice[0].reRoll = !playerTwoDice[0].reRoll;
-    console.log(playerTwoDice[0]);
-    let diceSelected = playerTwoDice[0];
-    display(diceSelected, selector);
-  } else if (diceID === "P2dice2") {
-    playerTwoDice[1].reRoll = !playerTwoDice[1].reRoll;
-    console.log(playerTwoDice[1]);
-    let diceSelected = playerTwoDice[1];
-    display(diceSelected, selector);
-  } else if (diceID === "P2dice3") {
-    playerTwoDice[2].reRoll = !playerTwoDice[2].reRoll;
-    console.log(playerTwoDice[2]);
-    let diceSelected = playerTwoDice[2];
-    display(diceSelected, selector);
-  } else if (diceID === "P2dice4") {
-    playerTwoDice[3].reRoll = !playerTwoDice[3].reRoll;
-    console.log(playerTwoDice[3]);
-    let diceSelected = playerTwoDice[3];
-    display(diceSelected, selector);
-  } else if (diceID === "P2dice5") {
-    playerTwoDice[4].reRoll = !playerTwoDice[4].reRoll;
-    console.log(playerTwoDice[4]);
-    let diceSelected = playerTwoDice[4];
-    display(diceSelected, selector);
+  if (playerOneTurn == true) {
+    if (diceID === "P1dice1") {
+      playerOneDice[0].reRoll = !playerOneDice[0].reRoll;
+      console.log(playerOneDice[0]);
+      let diceSelected = playerOneDice[0];
+      display(diceSelected, selector);
+    } else if (diceID === "P1dice2") {
+      playerOneDice[1].reRoll = !playerOneDice[1].reRoll;
+      console.log(playerOneDice[1]);
+      let diceSelected = playerOneDice[1];
+      display(diceSelected, selector);
+    } else if (diceID === "P1dice3") {
+      playerOneDice[2].reRoll = !playerOneDice[2].reRoll;
+      console.log(playerOneDice[2]);
+      let diceSelected = playerOneDice[2];
+      display(diceSelected, selector);
+    } else if (diceID === "P1dice4") {
+      playerOneDice[3].reRoll = !playerOneDice[3].reRoll;
+      console.log(playerOneDice[3]);
+      let diceSelected = playerOneDice[3];
+      display(diceSelected, selector);
+    } else if (diceID === "P1dice5") {
+      playerOneDice[4].reRoll = !playerOneDice[4].reRoll;
+      console.log(playerOneDice[4]);
+      let diceSelected = playerOneDice[4];
+      display(diceSelected, selector);
+    } else if (diceID === "P1dice6") {
+      playerOneDice[5].reRoll = !playerOneDice[5].reRoll;
+      console.log(playerOneDice[5]);
+      let diceSelected = playerOneDice[5];
+      display(diceSelected, selector);
+    }
+  } else {
+    if (diceID === "P2dice1") {
+      playerTwoDice[0].reRoll = !playerTwoDice[0].reRoll;
+      console.log(playerTwoDice[0]);
+      let diceSelected = playerTwoDice[0];
+      display(diceSelected, selector);
+    } else if (diceID === "P2dice2") {
+      playerTwoDice[1].reRoll = !playerTwoDice[1].reRoll;
+      console.log(playerTwoDice[1]);
+      let diceSelected = playerTwoDice[1];
+      display(diceSelected, selector);
+    } else if (diceID === "P2dice3") {
+      playerTwoDice[2].reRoll = !playerTwoDice[2].reRoll;
+      console.log(playerTwoDice[2]);
+      let diceSelected = playerTwoDice[2];
+      display(diceSelected, selector);
+    } else if (diceID === "P2dice4") {
+      playerTwoDice[3].reRoll = !playerTwoDice[3].reRoll;
+      console.log(playerTwoDice[3]);
+      let diceSelected = playerTwoDice[3];
+      display(diceSelected, selector);
+    } else if (diceID === "P2dice5") {
+      playerTwoDice[4].reRoll = !playerTwoDice[4].reRoll;
+      console.log(playerTwoDice[4]);
+      let diceSelected = playerTwoDice[4];
+      display(diceSelected, selector);
+    } else if (diceID === "P2dice6") {
+      playerTwoDice[5].reRoll = !playerTwoDice[5].reRoll;
+      console.log(playerTwoDice[5]);
+      let diceSelected = playerTwoDice[5];
+      display(diceSelected, selector);
+    }
   }
 }
 
@@ -174,14 +221,6 @@ function displayPlayerOne() {
   for (let i = 0; i < playerOneDice.length; i++) {
     let diceRoll = playerOneDice[i].number;
     let diceContainer = document.getElementById(`P1dice${i + 1}`);
-    if (started === true) {
-      diceContainer.innerHTML = '<img src="/assets/diceDeath.JPG" />';
-      diceContainer.innerHTML = '<img src="/assets/diceHeart.JPG" />';
-      diceContainer.innerHTML = '<img src="/assets/diceHelmet.JPG" />';
-      diceContainer.innerHTML = '<img src="/assets/diceShield.JPG" />';
-      diceContainer.innerHTML = '<img src="/assets/diceSword.JPG" />';
-      diceContainer.innerHTML = '<img src="/assets/diceArrow.JPG" />';
-    }
     if (diceRoll == 1) {
       diceContainer.innerHTML = '<img src="/assets/diceArrow.JPG" />';
     } else if (diceRoll == 2) {
@@ -202,14 +241,6 @@ function displayPlayerTwo() {
   for (let i = 0; i < playerTwoDice.length; i++) {
     let diceRoll = playerTwoDice[i].number;
     let diceContainer = document.getElementById(`P2dice${i + 1}`);
-    if (started === true) {
-      diceContainer.innerHTML = '<img src="/assets/diceDeath.JPG" />';
-      diceContainer.innerHTML = '<img src="/assets/diceHeart.JPG" />';
-      diceContainer.innerHTML = '<img src="/assets/diceHelmet.JPG" />';
-      diceContainer.innerHTML = '<img src="/assets/diceShield.JPG" />';
-      diceContainer.innerHTML = '<img src="/assets/diceSword.JPG" />';
-      diceContainer.innerHTML = '<img src="/assets/diceArrow.JPG" />';
-    }
     if (diceRoll == 1) {
       diceContainer.innerHTML = '<img src="/assets/diceArrow.JPG" />';
     } else if (diceRoll == 2) {
@@ -225,6 +256,3 @@ function displayPlayerTwo() {
     } else diceContainer.innerHTML = "error";
   }
 }
-
-displayPlayerOne();
-displayPlayerTwo();
